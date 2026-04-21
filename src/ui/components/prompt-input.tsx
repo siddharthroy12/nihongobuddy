4c2c5032586b016b57fc66d28a40eceb362d9bf5
Siddharth Roy <siddharthroy36912@gmail.com>
2026-04-21 13:42:24 +0530

    some changes

---
+++ src/ui/components/prompt-input.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/ui/components/ui/field";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+  InputGroupText,
+  InputGroupTextarea,
+} from "@/ui/components/ui/input-group";
+import { ArrowUpIcon, ImageIcon } from "lucide-react";
+
+export function PromptInput() {
+  return (
+    <Field>
+      <FieldLabel htmlFor="block-end-textarea">
+        Enter Text To Summarize The Sentence
+      </FieldLabel>
+      <InputGroup>
+        <InputGroupTextarea
+          id="block-end-textarea"
+          placeholder="Write a comment..."
+        />
+        <InputGroupAddon align="block-end">
+          <InputGroupButton variant="outline" size="sm" className="">
+            <ImageIcon />
+            Add Image
+          </InputGroupButton>
+          <InputGroupButton
+            variant="default"
+            size="icon-sm"
+            className="ml-auto"
+          >
+            <ArrowUpIcon />
+          </InputGroupButton>
+        </InputGroupAddon>
+      </InputGroup>
+    </Field>
+  );
+}
--- src/ui/router.tsx
+++ src/ui/router.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { createBrowserRouter } from "react-router";
 import Layout from "./layout";
-import Prompt from "./pages/prompt";
+import ExplainText from "./pages/explain-text";
 import { RouterProvider } from "react-router/dom";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ index: true, Component: Prompt }],
+    children: [{ index: true, Component: ExplainText }],
   },
 ]);
 
